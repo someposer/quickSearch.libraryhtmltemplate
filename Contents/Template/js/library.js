c93cb211beed5b55a1bf9f1ac2cd4817b052d738
someposer <someposer@users.noreply.github.com>
2013-12-30 14:27:47 -0700

    Adding Quick Search template files.

---
+++ Contents/Template/js/library.js
@@ -0,0 +1,47 @@
+$(window).load(function () {
+  $(document).ready(function(){
+    // Suppress form submission
+    $('form').submit(function(event){
+      event.preventDefault();
+    });
+    
+    // Move media out of the table and into the sieve
+    $('.medium').each(function(){
+      $('#sieve').append('<a class="list-group-item" href="#">'+$(this).html()+'</a>');
+    });
+    $('#mediaTable').remove();
+    $('#sieve').sieve({ itemSelector: 'a', searchInput: $('#searchInput') });
+    
+    // Fix the nav header
+    $('.shelf_selection option').each(function(){
+      $('#shelfList').append('<li><a href="'+$(this).val()+'">'+$(this).text()+'</a></li>')
+    });
+    $('.shelf_selection').remove();
+    
+    // Hide the items
+    $('.list-group-item-body').hide();
+    $('.list-group-item').click(function(event){
+      var active = $('.list-group-item.active');
+      if (active.length == 0) {
+        ShowDetails($(this));
+      } else {
+        var newActive = $(this);
+        active.removeClass('active').find('.list-group-item-body').slideUp('fast', function(){
+          if (active.get(0) != newActive.get(0)) {
+            ShowDetails(newActive);
+          }
+        });
+      }
+      
+      event.preventDefault();
+    });
+  });
+});
+
+function ShowDetails(e) {
+  e.addClass('active');
+  var details = e.find('.list-group-item-body');
+  if (details.find('img').length == 0)
+    details.prepend(details.comments().html());
+  details.slideDown('fast');
+}
